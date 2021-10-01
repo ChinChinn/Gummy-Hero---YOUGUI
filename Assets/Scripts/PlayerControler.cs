7ecda30c9daa51fa7c5f4277e11ccadcecd7e69b
LaLuluewe <pinkkbillyzucc07@gmail.com>
2021-10-01 16:05:34 -0500

    Codigo
    Avance de codigo.

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float speed;
+
+    private float input;
+
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate() {
+        input = Input.GetAxis("Horizontal");
+        Debug.Log("Input =" + input);
+        
+        rb.velocity = new Vector2 (input * speed, rb.velocity.y);
+    }
+}
+
