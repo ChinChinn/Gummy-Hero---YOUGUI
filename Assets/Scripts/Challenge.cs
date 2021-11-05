5e0807c0d21bb669514013008e5061314172279e
LaLuluewe <pinkkbillyzucc07@gmail.com>
2021-11-05 16:10:16 -0600

    Codigo
    Mas Codigo

---
+++ Assets/Scripts/Challenge.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Challenge : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string nombre = "Lulu";
+    public string apellido = "Vazquez";
+
+    
+  
+
+    void Start()
+    {
+        int i = 7;
+        int a = 4;
+        int c = a + i;
+
+
+        Debug.Log("Mi nombre es: " + nombre.ToUpper());
+        Debug.Log("Mi apellido es: " + apellido.ToLower());
+
+        Debug.Log("Mis iniciales de mi nombre son: " + nombre[0].ToString().ToUpper());
+        Debug.Log("Mis iniciales de mi apellido son: " + apellido[0].ToString().ToUpper());
+
+         Debug.Log(nombre + " tiene " + nombre.Length + " caracteres ");
+         Debug.Log(apellido + " tiene " + apellido.Length + " caracteres ");
+         Debug.Log("Ambos tienen en total " + c );            
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Fundamentos.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fundamentos : MonoBehaviour
+{
+     public int numero = 0; // Soy una variable entera
+     public float otroNumero = 5.0f; //Soy una varaible llamada float.
+
+    public bool state = false; //Soy una variable booleana.
+
+    public string nombre = "PerkZ";
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+         Debug.Log("Numero " + numero);
+         Debug.Log("Otro Numero " + otroNumero);
+         Debug.Log("State "+  state);
+         Debug.Log("Mi nombre es: " + nombre.ToUpper());
+         Debug.Log("Mi nombre es: " + nombre.ToLower());
+         Debug.Log("La primera letra es: " + nombre[0]);
+         Debug.Log(nombre + " tiene " + nombre.Length + " caracteres ");
+         Debug.Log("El ultimo caracter de " + nombre + " es " +  nombre.Substring(nombre.Length - 1, 1));
+        // [nombre.length-1] mas facil.
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
